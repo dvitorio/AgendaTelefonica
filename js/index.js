f25fcb5c21a2f16da0515ec39ac9a1556d5cbdf8
Daniel Vitorio <dvitorio2@gmail.com>
2022-05-12 23:40:42 -0300

    Inclusão do menu e da tela de novo contato

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function novoContato(){
+     var abrirTela = document.getElementById("novoContato");
+     abrirTela.style.display = 'block';
+}
+
+
+function fecharTela(){
+     var tela = document.getElementById("novoContato");
+     tela.style.display = 'none';
+}
