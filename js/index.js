77c1b3182a69d80a1a2526ad34f1ab566a9e3bad
Daniel Vitorio <dvitorio2@gmail.com>
2022-05-15 23:10:51 -0300

    Melhorias no menu de opções e construção das telas de edição de contato

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
+//ABRIR TELA DE NOVO CONTATO
 function novoContato(){
-     var abrirTela = document.getElementById("novoContato");
-     abrirTela.style.display = 'block';
+     var abrirTelaNovo = document.getElementById("novoContato");
+     abrirTelaNovo.style.display = 'block';
+     var fecharEditar = document.getElementById("menuEditarContato");
+     fecharEditar.style.display = 'none';
+     var fecharExcluir = document.getElementById("menuExcluirContato");
+     fecharExcluir.style.display = 'none';
+     var fecharNovoContato = document.getElementById("menuNovoContato");
+     fecharNovoContato.style.display = 'none';
+}
+
+//FECHAR TELA DE NOVO CONTATO
+function fecharTelaNovo(){
+     var telaNovo = document.getElementById("novoContato");
+     telaNovo.style.display = 'none';
+     var fecharEditar = document.getElementById("menuEditarContato");
+     fecharEditar.style.display = 'flex';
+     var fecharExcluir = document.getElementById("menuExcluirContato");
+     fecharExcluir.style.display = 'flex';
+     var fecharNovoContato = document.getElementById("menuNovoContato");
+     fecharNovoContato.style.display = 'flex';
+}
+
+//ABRIR TELA DE EDITAR CONTATO
+function editarContato(){
+     var abrirTelaEditar = document.getElementById("editarContato");
+     abrirTelaEditar.style.display = 'block';
+     var fecharEditar = document.getElementById("menuEditarContato");
+     fecharEditar.style.display = 'none';
+     var fecharExcluir = document.getElementById("menuExcluirContato");
+     fecharExcluir.style.display = 'none';
+     var fecharNovoContato = document.getElementById("menuNovoContato");
+     fecharNovoContato.style.display = 'none';
 }
 
 
-function fecharTela(){
-     var tela = document.getElementById("novoContato");
-     tela.style.display = 'none';
+function fecharAlteracoesContato(){
+     var telaAlteracoes = document.getElementById("editarContato");
+     telaAlteracoes.style.display = 'none'; 
+     var fecharEditar = document.getElementById("menuEditarContato");
+     fecharEditar.style.display = 'flex';
+     var fecharExcluir = document.getElementById("menuExcluirContato");
+     fecharExcluir.style.display = 'flex';
+     var fecharNovoContato = document.getElementById("menuNovoContato");
+     fecharNovoContato.style.display = 'flex';
 }
